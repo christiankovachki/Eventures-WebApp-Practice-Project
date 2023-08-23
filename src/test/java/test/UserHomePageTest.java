c3419f989881dcaab8a718858feb42195994cffe
Christian Kovachki <christian8251@gmail.com>
2023-08-23 19:25:26 +0300

    Removed unnecessary commentating

--- src/test/java/test/UserHomePageTest.java
+++ src/test/java/test/UserHomePageTest.java
@@ -13,9 +13,9 @@ import pages.LoginPage;
 import pages.UserHomePage;
 
 public class UserHomePageTest {
-    WebDriver driver;
-    LoginPage loginPage;
-    UserHomePage userHomePage;
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private UserHomePage userHomePage;
 
     @BeforeMethod
     public void setUp() {
@@ -26,56 +26,56 @@ public class UserHomePageTest {
         driver.manage().window().maximize();
     }
 
-//    @Test
-//    public void testAllEventsFromDropdown() {
-//        loginPage.loginSteps("guest", "guest");
-//        userHomePage.clickEventsMenu();
-//        userHomePage.clickAllEventsFromDropdown();
-//
-//        Assert.assertEquals(driver.getTitle(), "All Events - Eventures App");
-//        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
-//    }
-//
-//    @Test
-//    public void testAllEventsFromLink() {
-//        loginPage.loginSteps("guest", "guest");
-//        userHomePage.clickAllEventsFromLink();
-//
-//        Assert.assertEquals(driver.getTitle(), "All Events - Eventures App");
-//        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
-//    }
-//
-//    @Test
-//    public void testCreateEventFromDropdown() {
-//        loginPage.loginSteps("guest", "guest");
-//        userHomePage.clickEventsMenu();
-//        userHomePage.clickCreateEventFromDropdown();
-//
-//        Assert.assertEquals(driver.getTitle(), "Create Event - Eventures App");
-//        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
-//                Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/form/div/div[4]/div/input")).isDisplayed());
-//    }
-//
-//    @Test
-//    public void testCreateEventFromLink() {
-//        loginPage.loginSteps("guest", "guest");
-//        userHomePage.clickCreateFromLink();
-//
-//        Assert.assertEquals(driver.getTitle(), "Create Event - Eventures App");
-//        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
-//        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/form/div/div[4]/div/input")).isDisplayed());
-//    }
+    @Test(priority = 0)
+    public void testAllEventsFromDropdown() {
+        loginPage.loginSteps("guest", "guest");
+        userHomePage.clickEventsMenu();
+        userHomePage.clickAllEventsFromDropdown();
 
-//    @Test
-//    public void testLogoutButton() {
-//        loginPage.loginSteps("guest", "guest");
-//        userHomePage.clickLogoutLink();
-//
-//        Assert.assertTrue(driver.findElement(By.xpath("/html/body/header/nav/div/div/ul[1]/li[2]/a")).isDisplayed());
-//    }
+        Assert.assertEquals(driver.getTitle(), "All Events - Eventures App");
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
+    }
+
+    @Test(priority = 1)
+    public void testAllEventsFromLink() {
+        loginPage.loginSteps("guest", "guest");
+        userHomePage.clickAllEventsFromLink();
+
+        Assert.assertEquals(driver.getTitle(), "All Events - Eventures App");
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
+    }
+
+    @Test(priority = 2)
+    public void testCreateEventFromDropdown() {
+        loginPage.loginSteps("guest", "guest");
+        userHomePage.clickEventsMenu();
+        userHomePage.clickCreateEventFromDropdown();
+
+        Assert.assertEquals(driver.getTitle(), "Create Event - Eventures App");
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/form/div/div[4]/div/input")).isDisplayed());
+    }
+
+    @Test(priority = 3)
+    public void testCreateEventFromLink() {
+        loginPage.loginSteps("guest", "guest");
+        userHomePage.clickCreateFromLink();
+
+        Assert.assertEquals(driver.getTitle(), "Create Event - Eventures App");
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/h1")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div/main/form/div/div[4]/div/input")).isDisplayed());
+    }
+
+    @Test(priority = 4)
+    public void testLogoutButton() {
+        loginPage.loginSteps("guest", "guest");
+        userHomePage.clickLogoutLink();
+
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/header/nav/div/div/ul[1]/li[2]/a")).isDisplayed());
+    }
 
     @AfterMethod
     public void tearDown() {
-        //driver.quit();
+        driver.quit();
     }
 }
