19023b6ab687f95163985dc038c9d1d8f8647c2e
Christian Kovachki <christian8251@gmail.com>
2023-09-11 22:05:09 +0300

    Updated RegisterPage class with new methods

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -6,14 +6,20 @@ import org.openqa.selenium.WebDriver;
 public class RegisterPage {
     private WebDriver driver;
     private HomePage homePage;
-    private final String pageTitle = "Register - Eventures App";
     private By usernameField = By.id("Input_Username");
     private By emailField = By.id("Input_Email");
     private By passwordField = By.id("Input_Password");
     private By confirmPasswordField = By.id("Input_ConfirmPassword");
     private By firstNameField = By.id("Input_FirstName");
     private By lastNameField = By.id("Input_LastName");
-    private By registerButton = By.xpath("/html/body/div/main/div/div/form/button");
+    private By registerButton = By.xpath("//button[contains(.,'Register')]");
+    private By invalidFirstNameMessage = By.id("Input_FirstName-error");
+    private By invalidLastNameMessage = By.id("Input_LastName-error");
+    private By invalidPasswordMessage = By.id("Input_Password-error");
+    private By invalidConfirmPasswordMessage = By.id("Input_ConfirmPassword-error");
+    private By invalidUsernameMessage = By.id("Input_Username-error");
+    private By invalidEmailMessage = By.id("Input_Email-error");
+
 
     public RegisterPage(WebDriver driver) {
         this.driver = driver;
@@ -47,8 +53,28 @@ public class RegisterPage {
         driver.findElement(registerButton).click();
     }
 
-    public String getPageTitle() {
-        return pageTitle;
+    public String getInvalidFirstNameMessage() {
+        return driver.findElement(invalidFirstNameMessage).getText();
+    }
+
+    public String getInvalidLastNameMessage() {
+        return driver.findElement(invalidLastNameMessage).getText();
+    }
+
+    public String getInvalidPasswordMessage() {
+        return driver.findElement(invalidPasswordMessage).getText();
+    }
+
+    public String getInvalidConfirmPasswordMessage() {
+        return driver.findElement(invalidConfirmPasswordMessage).getText();
+    }
+
+    public String getInvalidUsernameMessage() {
+        return driver.findElement(invalidUsernameMessage).getText();
+    }
+
+    public String getInvalidEmailMessage() {
+        return driver.findElement(invalidEmailMessage).getText();
     }
 
     public void registrationSteps(String username, String email, String password, String confirmPassword,
